439aed52ec4501b4761467b6db35c7d939f07170
Dylan Terrell <dylancodes.io@gmail.com>
2017-05-27 22:58:41 -0400

    Updated files

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
-var app = angular.module("app", []);
+var app = angular.module('app', []);
 
-app.controller("mainCtrl", function($scope, $http) {
+app.controller('mainCtrl', ['$scope', '$http', '$injector', function($scope, $http, $injector) {
 
-});
+  var url = "https://data.cityofnewyork.us/resource/9w7m-hzhe.json";
+
+
+  $scope.eatify = function() {
+
+    $http.get('https://data.cityofnewyork.us/resource/9w7m-hzhe.json?dba=' + $scope.dba + '&street=' + $scope.street + '&zipcode=' + $scope.zip + '')
+    .then( function(response) {
+      //Handles Success
+      $scope.$parent.info = response.data;
+      console.log($scope.$parent.info);
+    }, function(failed) {
+      //Handles Error
+      $scope.error = "Could not fetch data. Please try again.";
+    });
+
+  }
+
+}]);
